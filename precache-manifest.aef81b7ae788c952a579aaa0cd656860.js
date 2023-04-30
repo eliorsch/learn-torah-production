270376ce54658ae35f8ac7c778002ab7c45c335c
eliorsch <109105841+eliorsch@users.noreply.github.com>
2023-04-30 13:40:14 +0300

    Add files via upload

---
+++ precache-manifest.aef81b7ae788c952a579aaa0cd656860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "965faae56668742a0c8b",
+    "url": "/static/css/main.5e903753.chunk.css"
+  },
+  {
+    "revision": "965faae56668742a0c8b",
+    "url": "/static/js/main.965faae5.chunk.js"
+  },
+  {
+    "revision": "6ee71e09cc840730ff54",
+    "url": "/static/js/1.6ee71e09.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9fd0164f59be6263f111ac951831fb6b",
+    "url": "/static/media/siteLogo.9fd0164f.jpg"
+  },
+  {
+    "revision": "b23b21454cfcaef1d99d23c8929db75a",
+    "url": "/index.html"
+  }
+];
